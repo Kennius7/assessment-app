c4fa52c6d9049fc739a00833a8e2665cf15f4837
Kennius7 <ogbogukenny@yahoo.com>
2025-04-03 08:55:53 +0100

    feat: make UI changes

--- src/app/edit/[id]/page.tsx
+++ src/app/edit/[id]/page.tsx
@@ -1,43 +1,30 @@
 "use client";
 
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable react-hooks/exhaustive-deps */
-import { useState, useRef, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useParams } from "next/navigation";
 import { useRouter } from 'next/navigation';;
 import axios from "axios";
-import { Container, CircularProgress, Typography, Card } from '@mui/material';
+import { Container, CircularProgress } from '@mui/material';
 import ModalBox from "@/app/modal";
+import Image from "next/image";
 
 
 const ImageEdit = () => {
     const [image, setImage] = useState<HTMLImageElement | null>(null);
     const [loading, setLoading] = useState(true);
     const [dimensions, setDimensions] = useState({ width: 500, height: 300 });
-    // const [imagePosition, setImagePosition] = useState({ x: 0, y: 0 });
-    // const [selected, setSelected] = useState(false);
-    const { id } = useParams();
-    const router = useRouter();
-
-    const canvasRef = useRef<HTMLCanvasElement | null>(null);
-    const [isDragging, setIsDragging] = useState(false);
-    const [position, setPosition] = useState({ x: 0, y: 0 });
-    const [scale, setScale] = useState(1);
-    const [angle, setAngle] = useState(0);
-    const [mouseStart, setMouseStart] = useState({ x: 0, y: 0 });
-
-    // const stageRef = useRef<any>(null);
     const [open, setOpen] = useState(false);
-    const [modalWidth, setModalWidth] = useState(0);
     const [grayscale, setGrayscale] = useState(false);
     const [blur, setBlur] = useState(0);
 
+    const { id } = useParams();
+    const router = useRouter();
+
     useEffect(() => {
         if (typeof window !== "undefined") {
-            setModalWidth(window.innerWidth > 500 ? (dimensions.width * 2) : (dimensions.width * 1.4));
             setDimensions({ width: window.innerWidth > 500 ? 500 : 280, height: window.innerWidth > 500 ? 300 : 180 });
         }
-    }, [])
+    }, [dimensions.width])
 
     const fetchImage = async () => {
         const imageURL = `https://picsum.photos/id/${id}`;
@@ -68,53 +55,8 @@ const ImageEdit = () => {
     useEffect(() => { 
         if (!id) return;
         fetchImage() 
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [id, grayscale, blur]);
-
-    useEffect(() => {
-        if (!image || !canvasRef.current) return;
-
-        const canvas = canvasRef.current;
-        const ctx = canvas.getContext("2d");
-        if (!ctx) return;
-
-        canvas.width = 500;
-        canvas.height = 300;
-
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.save();
-        ctx.translate(position.x + image.width / 2, position.y + image.height / 2);
-        ctx.rotate(angle * (Math.PI / 180));
-        ctx.scale(scale, scale);
-        ctx.drawImage(image, -image.width / 2, -image.height / 2);
-        ctx.restore();
-    }, [image, position, scale, angle]);
-
-    const handleMouseDown = (e: React.MouseEvent) => {
-        setIsDragging(true);
-        setMouseStart({ x: e.clientX, y: e.clientY });
-    };
-
-    const handleMouseMove = (e: React.MouseEvent) => {
-        if (!isDragging) return;
-
-        setPosition(prev => ({
-            x: prev.x + (e.clientX - mouseStart.x),
-            y: prev.y + (e.clientY - mouseStart.y),
-        }));
-        setMouseStart({ x: e.clientX, y: e.clientY });
-    };
-
-    const handleMouseUp = () => {
-        setIsDragging(false);
-    };
-
-    const handleScaleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setScale(parseFloat(event.target.value));
-    };
-
-    const handleRotateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setAngle(parseFloat(event.target.value));
-    };
 
     const handleImageDownload = async () => {
         if (!image?.src) {
@@ -144,55 +86,9 @@ const ImageEdit = () => {
     const handleClose = () => { setOpen(false) };
 
 
-
-
-
     return (
-        <div className="w-full h-[100dvh] flex flex-col justify-center items-center">
-            <div className='w-full flex justify-center items-center sm:mb-2 mb-0'>
-                <Typography variant="h5" component="h2" gutterBottom>
-                    EDIT IMAGE
-                </Typography>
-            </div>
-            <div className='w-full flex justify-center items-center sm:p-0 p-2'>
-                <Card 
-                    sx={{ 
-                        display: "flex", 
-                        justifyContent: "center", 
-                        alignItems: "center", 
-                        width: dimensions.width, 
-                        height: dimensions.height,
-                    }}
-                >
-                    {
-                        loading ? (
-                            <Container 
-                                sx={{ 
-                                    display: "flex", 
-                                    justifyContent: "center", 
-                                    alignItems: "center", 
-                                    width: "100%",
-                                    height: "100%" 
-                                }}
-                            >
-                                <CircularProgress />
-                            </Container>
-                        ) : (
-                            <div>
-                                <canvas 
-                                    ref={canvasRef}
-                                    onMouseDown={handleMouseDown}
-                                    onMouseMove={handleMouseMove}
-                                    onMouseUp={handleMouseUp}
-                                    onMouseLeave={handleMouseUp}
-                                    className="border cursor-pointer"
-                                />
-                            </div>
-                        )
-                    }
-                </Card>
-            </div>
-            <div className='sm:w-[35%] w-[96%] flex justify-between items-center mt-4'>
+        <div className="w-full h-[100dvh] flex flex-col justify-start items-center sm:py-[100px] py-[10px]">
+            <div className='w-[78%] flex justify-start items-center mb-[100px]'>
                 <button
                     className="bg-purple-700 text-white sm:px-8 px-4 py-2 rounded sm:w-[140px] w-[100px] 
                     cursor-pointer sm:text-[14px] text-[12px]"
@@ -200,6 +96,39 @@ const ImageEdit = () => {
                 >
                     ← Go Back
                 </button>
+            </div>
+            <div className='w-full flex justify-center items-center sm:mb-2 mb-0'>
+                <div className='w-full mb-4 sm:text-[25px] text-[20px] font-bold text-center'>
+                    IMAGE PREVIEW
+                </div>
+            </div>
+            <div className='w-full flex justify-center items-center sm:p-0 p-2'>
+                {
+                    loading ? (
+                        <Container 
+                            sx={{ 
+                                display: "flex", 
+                                justifyContent: "center", 
+                                alignItems: "center", 
+                                width: "100%",
+                                height: "100%" 
+                            }}
+                        >
+                            <CircularProgress />
+                        </Container>
+                    ) : (
+                        <div className='w-full h-full flex justify-center items-center overflow-hidden'>
+                            <Image 
+                                src={image!.src}
+                                alt="Image" 
+                                width={dimensions.width} 
+                                height={dimensions.height} 
+                            />
+                        </div>
+                    )
+                }
+            </div>
+            <div className='sm:w-[35%] w-[78%] flex justify-between items-center mt-4'>
                 <button
                     className="bg-blue-500 text-white sm:px-8 px-4 py-2 rounded sm:w-[140px] w-[100px] 
                     cursor-pointer sm:text-[14px] text-[12px]"
@@ -219,7 +148,6 @@ const ImageEdit = () => {
             <ModalBox 
                 open={open} 
                 onClose={handleClose}
-                modalWidth={modalWidth}
                 settingHeight={dimensions.height}
                 settingWidth={dimensions.width}
                 setGrayscale={setGrayscale}
--- src/app/modal/index.tsx
+++ src/app/modal/index.tsx
@@ -1,23 +1,19 @@
 "use client";
-/* eslint-disable @typescript-eslint/no-unused-vars */
 
 import React, { useState, useEffect, useRef } from 'react';
-import { Container, Typography, Box, Button, Modal } from '@mui/material';
+import { Modal } from '@mui/material';
 import EditSection from "../edit/[id]/editSection";
 import { ParamValue } from 'next/dist/server/request/params';
 
 
-
-
 const ModalBox = ({ 
-    open, onClose, modalWidth, settingHeight, settingWidth, setGrayscale, grayscale, 
+    open, onClose, settingHeight, settingWidth, setGrayscale, grayscale, 
     setBlur, blur, setDimensions, dimensions, image, id,
 }: {
     open: boolean,
     onClose: () => void,
     dimensions: { width: number, height: number },
     setDimensions: ({ width, height }: { width: number, height: number }) => void,
-    modalWidth: number,
     settingHeight: number,
     settingWidth: number,
     setGrayscale: (value: boolean) => void, 
@@ -33,7 +29,6 @@ const ModalBox = ({
     const [position, setPosition] = useState({ x: 0, y: 0 });
     const [scale, setScale] = useState(1);
     const [angle, setAngle] = useState(0);
-    // const [mouseStart, setMouseStart] = useState({ x: 0, y: 0 });
     const mouseStartRef = useRef({ x: 0, y: 0 });
 
 
@@ -48,13 +43,10 @@ const ModalBox = ({
         canvas.height = dimensions.height;
 
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        // ctx.drawImage(image, -image.width / 2, -image.height / 2);
-
-        ctx.save(); // Save current state
-        ctx.translate(canvas.width / 2 + position.x, canvas.height / 2 + position.y); // Move center
-        ctx.rotate((angle * Math.PI) / 180); // Rotate
-        ctx.scale(scale, scale); // Scale
-    
+        ctx.save();
+        ctx.translate(canvas.width / 2 + position.x, canvas.height / 2 + position.y);
+        ctx.rotate((angle * Math.PI) / 180);
+        ctx.scale(scale, scale);
         ctx.drawImage(image, -image.width / 2, -image.height / 2);
         ctx.restore(); 
     }, [image, position, scale, angle, dimensions.width, dimensions.height]);
@@ -64,11 +56,8 @@ const ModalBox = ({
             console.error("Canvas reference is missing!");
             return;
         }
-        // console.log("Stage Ref:>>>>>>", canvasRef.current);
-        // canvasRef.current.draw();
         setTimeout(() => {
             const dataURL = canvasRef?.current?.toDataURL();
-            // console.log("Data URL:>>>>", dataURL);
             const link = document.createElement("a");
             if (dataURL && link) {
                 link.href = dataURL;
@@ -80,7 +69,6 @@ const ModalBox = ({
 
     const handleMouseDown = (e: React.MouseEvent) => {
         setIsDragging(true);
-        // setMouseStart({ x: e.clientX, y: e.clientY });
         mouseStartRef.current = { x: e.clientX, y: e.clientY };
     };
 
@@ -91,7 +79,6 @@ const ModalBox = ({
             x: prev.x + (e.clientX - mouseStartRef.current.x),
             y: prev.y + (e.clientY - mouseStartRef.current.y),
         }));
-        // setMouseStart({ x: e.clientX, y: e.clientY });
         mouseStartRef.current = { x: e.clientX, y: e.clientY };
         console.log("Position:>>>>", position);
     };
@@ -106,64 +93,38 @@ const ModalBox = ({
     
     const handleTouchMove = (e: React.TouchEvent) => {
         if (!isDragging) return;
-    
         const touch = e.touches[0];
     
         setPosition(prev => ({
             x: prev.x + (touch.clientX - mouseStartRef.current.x),
             y: prev.y + (touch.clientY - mouseStartRef.current.y),
         }));
-    
         mouseStartRef.current = { x: touch.clientX, y: touch.clientY };
         console.log("Position:>>>>", position);
     };
     
     const handleTouchEnd = () => { setIsDragging(false) };
 
-    // const handleScaleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    //     setScale(parseFloat(event.target.value));
-    // };
 
-    // const handleRotateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    //     setAngle(parseFloat(event.target.value));
-    // };
-
-    const modalStyle = {
-        position: "absolute",
-        top: "50%",
-        left: "50%",
-        transform: "translate(-50%, -50%)",
-        // bgcolor: "background.paper",
-        bgcolor: "#bbb",
-        border: "2px solid #000",
-        borderRadius: 2,
-        boxShadow: 24,
-        p: window.innerWidth > 500 ? 4 : 2,
-        width: window.innerWidth > 500 ? modalWidth : "98%",
-        height: window.innerWidth > 500 ? "80%" : "90%",
-        // maxWidth: 800,
-        display: "flex", 
-        flexDirection: "column", 
-        justifyContent: "center", 
-        alignItems: "center",
-    };
 
     return (
         <Modal open={open} onClose={onClose}>
             <div 
                 style={{ transform: "translate(-50%, -50%)" }} 
-                className='absolute top-[50%] left-[50%] sm:w-[600px] w-[98%] sm:h-[80%] h-[90%] 
+                className='absolute top-[50%] left-[50%] sm:w-[600px] w-[98%] sm:h-[80%] h-fit 
                 flex flex-col justify-center items-center bg-slate-200 rounded-[3px] 
                 border-slate-700 border-[2px] sm:p-4 p-2'
             >
-                <div className='sm:text-[20px] text-[18px] font-semibold'>
-                    Edit Image
+                <div className='w-full flex justify-start items-center'>
+                    <div className='sm:text-[20px] text-[18px] font-semibold text-start ml-2'>
+                        Edit Image
+                    </div>
                 </div>
     
                 <div className='w-full h-full flex sm:flex-row flex-col sm:justify-between 
                     justify-center items-center m-1'>
                     <div className='sm:w-full w-[98%] sm:h-full h-[200px] flex justify-center 
-                        items-center overflow-hidden border-2 border-black'>
+                        items-center overflow-hidden border-2 border-slate-800'>
                         <canvas 
                             ref={canvasRef}
                             onMouseDown={handleMouseDown}
@@ -173,7 +134,7 @@ const ModalBox = ({
                             onTouchStart={handleTouchStart}
                             onTouchMove={handleTouchMove}
                             onTouchEnd={handleTouchEnd}
-                            className="border cursor-pointer touch-none"
+                            className="border cursor-pointer w-full h-full touch-none"
                         />
                     </div>
                     <EditSection 
